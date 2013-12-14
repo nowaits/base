41afde582c697752a6491c3787e4229828077d08
nowaits <nowaits@96f50dd2-2846-244b-9c2c-1aac2c5b0ef2>
2013-12-14 18:41:23 +0000

    test
    git-svn-id: https://nowaits/svn/base@7 96f50dd2-2846-244b-9c2c-1aac2c5b0ef2

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include "base\memory\singleton.h"
 #include "base\at_exit.h"
 #include "base\compiler_specific.h"
+#include "base\memory\weak_ptr.h"
+#include "base\bind.h"
+#include "bind_helpers.h"
 
-class Test {
+class SingletonTest {
 public:
-  Test() {}
-  static Test* GetInstance(){
-    return Singleton<Test>::get();
+  SingletonTest() {}
+  ~SingletonTest(){}
+
+  static SingletonTest* GetInstance(){
+    return Singleton<SingletonTest>::get();
   };
-  
-private:
-  char x_[29];
 };
 
 /*
@@ -20,9 +22,42 @@ private:
           assert(((int)&data & (int)(4096 - 1)) == 0);
 */
 
-int main() {
-  base::AtExitManager s;
-  Test* t = Test::GetInstance();
+class WeakPtrTest {
+public:
+  WeakPtrTest() : weak_ref_(this){}
+
+  ~WeakPtrTest(){}
+
+  base::WeakPtr<WeakPtrTest> AsWeakPtr() { 
+    return weak_ref_.GetWeakPtr();
+  }
   
+  int WeakCallBackTestA() {
+    return 43;
+  }
+
+  static int WeakFun(base::WeakPtr<WeakPtrTest> weak, int default){
+    if (weak.get())
+      return weak->WeakCallBackTestA();
+    else 
+      return default;
+  }
+  void WeakCallBackTestB() {
+    return;
+  }
+private:
+  base::WeakPtrFactory<WeakPtrTest> weak_ref_;
+};
+
+int main() {
+  base::AtExitManager g_exit;
+  SingletonTest* t = SingletonTest::GetInstance();
+  WeakPtrTest* ptr = new WeakPtrTest;
+
+  base::Callback<int()> call_back = 
+  base::Bind(&WeakPtrTest::WeakFun, ptr->AsWeakPtr(), 32);
+  assert(call_back.Run() != 32);
+  delete ptr;
+  assert(call_back.Run() == 32);
   return 0;
 }
