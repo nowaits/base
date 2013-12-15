fac38a1eaa3843aace0063b454a09c09f69afa06
nowaits <nowaits@96f50dd2-2846-244b-9c2c-1aac2c5b0ef2>
2013-12-15 04:05:20 +0000

    添加测试模块
    git-svn-id: https://nowaits/svn/base@8 96f50dd2-2846-244b-9c2c-1aac2c5b0ef2

---
+++ all_test.cpp
@@ -0,0 +1,64 @@
+#include "base\memory\singleton.h"
+#include "base\at_exit.h"
+#include "base\compiler_specific.h"
+#include "base\memory\weak_ptr.h"
+#include "base\bind.h"
+#include "bind_helpers.h"
+#include "base\test\test_all.h"
+class SingletonTest {
+public:
+  SingletonTest() {}
+  ~SingletonTest(){}
+
+  static SingletonTest* GetInstance(){
+    return Singleton<SingletonTest>::get();
+  };
+};
+
+/*
+          1.POD数据类型
+          2.内存对齐
+          ALIGNAS(4096) uint8 data;
+          assert(((int)&data & (int)(4096 - 1)) == 0);
+*/
+
+TEST(SingletonTest) {
+  SingletonTest* t = SingletonTest::GetInstance();
+}
+
+class WeakPtrTest {
+public:
+  WeakPtrTest() : weak_ref_(this){}
+
+  ~WeakPtrTest(){}
+
+  base::WeakPtr<WeakPtrTest> AsWeakPtr() { 
+    return weak_ref_.GetWeakPtr();
+  }
+  
+  int WeakCallBackTestA() {
+    return 43;
+  }
+
+  static int WeakFun(base::WeakPtr<WeakPtrTest> weak, int default){
+    if (weak.get())
+      return weak->WeakCallBackTestA();
+    else 
+      return default;
+  }
+  void WeakCallBackTestB() {
+    return;
+  }
+private:
+  base::WeakPtrFactory<WeakPtrTest> weak_ref_;
+};
+
+TEST(WeakPtrTest) {
+  WeakPtrTest* ptr = new WeakPtrTest;
+
+  base::Callback<int()> call_back = 
+    base::Bind(&WeakPtrTest::WeakFun, ptr->AsWeakPtr(), 32);
+  assert(call_back.Run() != 32);
+  delete ptr;
+  assert(call_back.Run() == 32);
+}
---
+++ test/test_all.h
@@ -0,0 +1,66 @@
+/*
+简单的测试模板
+用法：
+TEST(test_case_name) {
+.....
+}
+*/
+
+#pragma once
+#include <list>
+
+class TestBase {
+public:
+  virtual void TestBody() = 0;
+};
+
+template <typename T>
+class TestList {
+public:
+  static TestList<T>* GetInstance() {
+    static TestList<T> instance;
+    return &instance;
+  }
+
+  void Add(T* test_body) {
+    list_.push_back(test_body);
+  }
+
+  void Remove(T* test_body) {
+    list_.remove(test_body);
+  }
+  void Run() {
+    std::list<T*>::const_iterator it  = list_.begin();
+    std::list<T*>::const_iterator end = list_.end();
+
+    while(it != end) {
+      (*it)->TestBody();
+      it ++;
+    }
+  }
+private:
+  std::list<T*> list_;
+};
+
+#define GTEST_DISALLOW_COPY_AND_ASSIGN(type)\
+  type(const type &);\
+  void operator=(const type &)
+
+#define RUN_ALL_TEST() TestList<TestBase>::GetInstance()->Run()
+
+#define TEST(test_case_name)\
+class test_case_name##_Test : public TestBase {\
+public:\
+  test_case_name##_Test() {;\
+  TestList<TestBase>::GetInstance()->Add(this);\
+}\
+  ~test_case_name##_Test() {;\
+  TestList<TestBase>::GetInstance()->Remove(this);\
+}\
+  virtual void TestBody();\
+private:\
+  static const test_case_name##_Test test_init_;\
+  GTEST_DISALLOW_COPY_AND_ASSIGN(test_case_name##_Test);\
+};\
+  const test_case_name##_Test test_case_name##_Test::test_init_;\
+  void test_case_name##_Test::TestBody()
