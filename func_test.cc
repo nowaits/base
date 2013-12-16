35bba4ac9818d9cb3a6468784a0484da0fa7b1ba
jxm <jxm@96f50dd2-2846-244b-9c2c-1aac2c5b0ef2>
2013-12-16 07:54:31 +0000

    函数类型判断模板
    git-svn-id: https://nowaits/svn/base@23 96f50dd2-2846-244b-9c2c-1aac2c5b0ef2

---
+++ func_test.cc
@@ -0,0 +1,57 @@
+#include "base\func_type.h"
+#include "base\test\unit_test.h"
+#include <string>
+#include <assert.h>
+
+class TestClass {
+public:
+  static void funa(){};
+  void funb(){};
+};
+
+void fun(){*((char*)0) = 0;}
+void funa(int){*((char*)0) = 0;}
+void funaa(std::string){*((char*)0) = 0;}
+void funb(int, int){*((char*)0) = 0;}
+void funbb(int, std::string){*((char*)0) = 0;}
+void func(int, int, int){*((char*)0) = 0;}
+void funcc(std::string, int, std::string){*((char*)0) = 0;}
+
+int fun1(){*((char*)0) = 0;return 0;}
+bool fund(int){*((char*)0) = 0;return 0;}
+bool fundd(std::string){*((char*)0) = 0;return 0;}
+bool fune(int, int){*((char*)0) = 0;return 0;}
+bool funee(int, std::string){*((char*)0) = 0;return 0;}
+bool funf(int, int, int){*((char*)0) = 0;return 0;}
+bool funff(std::string, std::string, int){*((char*)0) = 0;return 0;}
+
+typedef char Type1[1];
+typedef char Type2[2];
+
+Type1& Check(...);
+Type2& Check(void(*)());
+
+UNIT_TEST(funtype) {
+
+  const bool a = IsVoidFun(fun);
+  const int  aa = FunArgNum(fun);
+  const bool b = IsVoidFun(funb);
+  const int  bb = FunArgNum(funb);
+  const bool c = IsVoidFun(funcc);
+  const int  cc = FunArgNum(funcc);
+
+  const int cdd = IsVoidFun(fun);
+  const int d = IsVoidFun(fun1);
+
+  const int e = sizeof(Check(fun));
+  const int f = sizeof(Check(fun1));
+
+  const bool g = FuncTypeDect<>::IsVoidFun(fun);
+  const bool h = FuncTypeDect<int>::IsVoidFun(funff);
+  const bool j = FuncTypeDect<bool>::IsVoidFun(funff);
+
+  assert(FuncTypeDect<>::IsVoidFun(fun) == true && FuncTypeDect<>::FunArgNum(fun) == 0);
+  assert(FuncTypeDect<>::IsFunction(a) == false);
+  assert(FuncTypeDect<int>::IsVoidFun(funff) == false);
+  assert(FuncTypeDect<bool>::IsVoidFun(funff) == true && FuncTypeDect<bool>::FunArgNum(funff) == 3);
+}
---
+++ func_type.h
@@ -0,0 +1,77 @@
+#pragma once
+
+template<typename T, typename R, T* v = 0>
+class FunType{
+  typedef char Type1[1];
+  typedef char Type2[2];
+  typedef char Type3[3];
+  typedef char Type4[4];
+  typedef char Type5[5];
+  typedef char Type6[6];
+  typedef char Type7[7];
+  typedef char Type8[8];
+  typedef char Type9[9];
+
+  static Type1& Check(...);
+
+  static Type2& Check(R(*)());
+
+  template<typename T1>
+  static Type3& Check(R(*)(T1));
+
+  template<typename T1, typename T2>
+  static Type4& Check(R(*)(T1, T2));
+
+  template<typename T1, typename T2, typename T3>
+  static Type5& Check(R(*)(T1, T2, T3));
+
+  template<typename T1, typename T2, typename T3, typename T4>
+  static Type6& Check(R(*)(T1, T2, T3, T4));
+
+  template<typename T1, typename T2, typename T3, typename T4, typename T5>
+  static Type7& Check(R(*)(T1, T2, T3, T4, T5));
+
+  template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
+  static Type8& Check(R(*)(T1, T2, T3, T4, T5, T6));
+
+  template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7>
+  static Type9& Check(R(*)(T1, T2, T3, T4, T5, T6, T7));
+
+public:
+  static const bool value = sizeof(Check(*v)) != sizeof(Type1);
+  static const int arg_num = sizeof(Check(*v)) - 2;
+};
+
+template<typename T>
+bool IsVoidFun(const T&){
+  return FunType<T, void>::value;
+}
+
+template<typename T>
+int FunArgNum(const T&){
+  return FunType<T, void>::arg_num;
+}
+
+template<typename T>
+bool IsIntFun(const T&){
+  return FunType<T, int>::value;
+}
+
+template<typename R = void>
+struct FuncTypeDect {
+
+  template<typename T>
+  static bool IsVoidFun(const T&){
+    return FunType<T, R>::value;
+  }
+
+  template<typename T>
+  static int FunArgNum(const T&){
+    return FunType<T, R>::arg_num;
+  }
+
+  template<typename T>
+  static bool IsFunction(const T&){
+    return FunType<T, R>::arg_num >= 0;
+  }
+};
